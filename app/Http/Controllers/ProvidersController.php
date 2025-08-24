33542066b7a5c1528616b5c14a8469718a0d8a9b
Anthony Bistocchi <anthonybistocchi.txt@gmail.com>
2025-08-24 18:13:23 -0300

    fix the name class of providers and the name of file providers

--- app/Http/Controllers/ProvidersController.php
+++ app/Http/Controllers/ProvidersController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class ProviderController extends Controller
+class ProvidersController extends Controller
 {
     public function providers()
     {
--- routes/web.php
+++ routes/web.php
@@ -7,16 +7,16 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ClientsController;
 use App\Http\Controllers\ProductsController;
-use App\Http\Controllers\ProviderController;
+use App\Http\Controllers\ProvidersController;
 
 
 route::get('/', [IndexController::class, 'index'])->name('index');
-route::get('/sobre-nos', [AboutUsController::class, 'aboutUs'])->name('about_us');
+route::get('/sobre-nos', [AboutUsController::class, 'aboutUs'])->name('aboutUs');
 route::get('/contato', [ContactController::class, 'contact'])->name('contact');
 route::get('/login', [LoginController::class, 'login'])->name('login');
 
 Route::prefix('/dashboard')->group(function () {
      route::get('/clientes', [ClientsController::class, 'clients'])->name('clients');
      route::get('/produtos', [ProductsController::class, 'products'])->name('products');
-     route::get('/fornecedores', [ProviderController::class, 'providers'])->name('providers');
+     route::get('/fornecedores', [ProvidersController::class, 'providers'])->name('providers');
 });
