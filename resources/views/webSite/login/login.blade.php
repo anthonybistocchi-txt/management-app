fc45db4bf2579053d8ff16ddb253c8a5e4278cb1
Anthony Bistocchi <anthonybistocchi.txt@gmail.com>
2025-09-26 20:38:12 -0300

    fix: fix the src of typescript

--- resources/views/webSite/login/login.blade.php
+++ resources/views/webSite/login/login.blade.php
@@ -49,7 +49,7 @@
 
     <!DOCTYPE html>
 <html lang="pt-BR">
-@vite(['resources/js/modals/modalError.ts'])
+
 
 <head>
     <meta charset="UTF-8">
@@ -87,7 +87,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
-<script></script>
+<script> src='js/login/index.ts'</script>
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
