224adf38c9fc4c22ee3c13ae26233f5dc29bf02f
Anthony Bistocchi <anthonybistocchi.txt@gmail.com>
2025-09-26 20:37:36 -0300

    fix: fix the src of typescript and add news id for validation

--- resources/views/webSite/contact/contact.blade.php
+++ resources/views/webSite/contact/contact.blade.php
@@ -75,5 +75,6 @@
   </footer>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+  <script> src="js/contact/index.ts"</script>
 </body>
 </html>
