902d53cea0de4de37f1ce92b1765f185d0ea2f7c
Anthony Bistocchi <anthonybistocchi.txt@gmail.com>
2025-10-19 11:35:16 -0300

    refac: refac arquitecture of project

--- routes/web.php
+++ routes/web.php
@@ -10,13 +10,4 @@ use App\Http\Controllers\ProductsController;
 use App\Http\Controllers\ProvidersController;
 
 
-route::get('/gestao-pro', [IndexController::class, 'index'])->name('index');
-route::get('/sobre-nos', [AboutUsController::class, 'aboutUs'])->name('aboutUs');
-route::get('/contato', [ContactController::class, 'contact'])->name('contact');
 route::get('/login', [LoginController::class, 'login'])->name('login');
-
-Route::prefix('/dashboard')->group(function () {
-     route::get('/clientes', [ClientsController::class, 'clients'])->name('clients');
-     route::get('/produtos', [ProductsController::class, 'products'])->name('products');
-     route::get('/fornecedores', [ProvidersController::class, 'providers'])->name('providers');
-});
