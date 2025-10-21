b51fc05db485c6fe31ab43f7237a11436df3f26f
Anthony <anthony.bistocchi@4x4brasil.com.br>
2025-10-21 13:23:30 -0300

    create new views em controllers

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AboutUsController;
-use App\Http\Controllers\ContactController;
+
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\IndexController;
-use App\Http\Controllers\ClientsController;
-use App\Http\Controllers\ProductsController;
-use App\Http\Controllers\ProvidersController;
+use App\Http\Controllers\RegisterController;
 
-
-route::get('/gestao-pro', [IndexController::class, 'index'])->name('index');
-route::get('/sobre-nos', [AboutUsController::class, 'aboutUs'])->name('aboutUs');
-route::get('/contato', [ContactController::class, 'contact'])->name('contact');
 route::get('/login', [LoginController::class, 'login'])->name('login');
-
-Route::prefix('/dashboard')->group(function () {
-     route::get('/clientes', [ClientsController::class, 'clients'])->name('clients');
-     route::get('/produtos', [ProductsController::class, 'products'])->name('products');
-     route::get('/fornecedores', [ProvidersController::class, 'providers'])->name('providers');
-});
+route::get('/cadastro',[RegisterController::class,'register'])->name('register');
+route::get('/gestao-pro', [IndexController::class, 'index'])->name('index');
