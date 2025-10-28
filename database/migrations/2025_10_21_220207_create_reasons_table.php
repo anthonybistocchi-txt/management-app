9dc7e4337a5b463d76f5a7530083fa938124812f
Anthony Bistocchi <anthonybistocchi.txt@gmail.com>
2025-10-28 09:29:00 -0300

    resolve conflicts

--- database/migrations/2025_10_21_220207_create_reasons_table.php
+++ database/migrations/2025_10_21_220207_create_reasons_table.php
@@ -11,16 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-<<<<<<<< HEAD:database/migrations/2025_10_21_220207_create_reasons_table.php
         Schema::create('reasons', function (Blueprint $table) {
             $table->id();
             $table->string('reason', 100);
             $table->timestamps();
-========
+        });
         Schema::create('type_user', function (Blueprint $table) {
             $table->id();
             $table->string('name');
->>>>>>>> eea00e57d13243300d71da2bb236a01c54c44eaa:database/migrations/2025_10_20_023656_add_type_user_table.php
         });
     }
 
@@ -29,10 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-<<<<<<<< HEAD:database/migrations/2025_10_21_220207_create_reasons_table.php
-      Schema::dropIfExists('reasons');
-========
-        //
->>>>>>>> eea00e57d13243300d71da2bb236a01c54c44eaa:database/migrations/2025_10_20_023656_add_type_user_table.php
+        Schema::dropIfExists('reasons');
     }
 };
