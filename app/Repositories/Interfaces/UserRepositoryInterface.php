ee99ed00d37031d54827069f231b9d81db22640e
anthonybistocchi-txt <anthonybistocchi.txt@gmail.com>
2025-12-07 11:37:12 -0300

    user interface

--- app/Repositories/Interfaces/UserRepositoryInterface.php
+++ app/Repositories/Interfaces/UserRepositoryInterface.php
@@ -4,9 +4,9 @@ namespace App\Repositories\Interfaces;
 
 interface UserRepositoryInterface 
 {
-    public function getUserById($id);
     public function getAllUsers();
     public function createUser(array $data);
     public function updateUser($id, array $data);
     public function deleteUser($id);
+    public function getUser(array $ids);
 }
