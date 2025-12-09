b005e34ec8c4fb13c8fbce1c35b0872f838e4c05
anthonybistocchi-txt <anthonybistocchi.txt@gmail.com>
2025-12-09 15:06:19 -0300

    created all files of each page the project

--- resources/ts/app.ts
+++ resources/ts/app.ts
@@ -6,16 +6,16 @@ import "./components/toast";
 
 // Auth pages
 import "./pages/login/login";
-import "./pages/login/createUser";
+import "./pages/login/create-user";
+import "./pages/login/reset-password";
 
 // Index pages
 import "./pages/index/dashboard";
 import "./pages/index/users";
 import "./pages/index/providers";
 import "./pages/index/products";
-import "./pages/index/stockIn";
-import "./pages/index/stockOut";
+import "./pages/index/stock-in";
+import "./pages/index/stock-out";
 import "./pages/index/movements";
-import "./pages/index/createUser";
-import "./pages/index/createProvider";
+import "./pages/index/users";
 
