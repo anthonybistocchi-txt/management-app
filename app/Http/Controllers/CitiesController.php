d43a3300f1d4ccb9811101ff841455e52467b8ae
anthonybistocchi-txt <anthonybistocchi.txt@gmail.com>
2026-04-04 01:24:19 -0300

    feat: adicionar busca de cidade por CEP

--- app/Http/Controllers/CitiesController.php
+++ app/Http/Controllers/CitiesController.php
@@ -5,12 +5,18 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\CitiesAllRequest;
 use App\Services\CitiesService;
+use Illuminate\Http\JsonResponse;
 
 class CitiesController extends Controller
 {
     public function __construct(protected CitiesService $citiesService){}
-    public function getAll(CitiesAllRequest $request)
+    public function getAll(CitiesAllRequest $request):JsonResponse
     {
         return $this->citiesService->getAllCities($request->validated()['uf_id']);
+    }
+
+    public function getCityByCEP(string $cep):JsonResponse
+    {
+        return $this->citiesService->getCityByCEP($cep);
     }
 }
--- app/Services/CitiesService.php
+++ app/Services/CitiesService.php
@@ -5,10 +5,31 @@ use Illuminate\Support\Facades\Http;
 
 class CitiesService
 {
-    public function getAllCities(string $uf)
+    public function __construct(protected CepService $cepService){}
+    public function getAllCities(string $uf): array
     {
-        return Http::get("https://servicodados.ibge.gov.br/api/v1/localidades/estados/$uf/municipios")
+        $cities = Http::get("https://servicodados.ibge.gov.br/api/v1/localidades/estados/$uf/municipios")
             ->json();
 
+        if (!$cities) {
+            throw new \Exception('No cities found for the provided state ID.', 404);
+        }
+
+        return $cities;
+    }
+
+    public function getCityByCEP(string $cep): array
+    {
+        $cepData = $this->cepService->getAddressByCep($cep);
+
+        if (!$cepData) {
+            throw new \Exception('No city found for the provided CEP.', 404);
+        }
+
+      return [
+            'city'  => $cepData['localidade'] ?? null,
+            'uf'    => $cepData['uf']         ?? null,
+            'state' => $cepData['estado']     ?? null,
+        ];
     }
 }
